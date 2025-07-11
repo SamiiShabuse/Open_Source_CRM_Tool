dd2ac5c7526ef43782c525c64ded29fc46ac51f9
Samii Shabuse <sshabuse@gmail.com>
2025-07-11 18:26:48 -0400

    refactor: hiding django secret key

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gl+k2u+o5zkf59sgnts(smtu@#2-6tv%+3^k^nmn+m3z1s5+ea'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
